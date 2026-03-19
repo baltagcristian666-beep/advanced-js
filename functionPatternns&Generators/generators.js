155c0b4a585f9815f159bf261e396dccdfcfc2a3
baltagcristian666-beep <baltagcristian666@gmail.com>
2026-03-19 03:06:42 +0200

    q

---
+++ functionPatternns&Generators/generators.js
@@ -0,0 +1,9 @@
+//Generators
+
+/*
+Purpose: Used to unsure a function is called at most once in a period of time.
+Mechanism: Functions that can be paused and resumed. Produce a sequence of
+values.
+Use case: async functions, handling state, lazy evaluation
+Return: A response such as async functions
+*/
--- functionPatternns&Generators/throttling&debouncing.js
+++ functionPatternns&Generators/throttling&debouncing.js
@@ -25,11 +25,51 @@ Purpose: Used to unsure a function is called at most once in a period of time.
 Mechanism: When an event triggers multiple times. Throtteling calls the function once 
 at specific time intervals ignoring the aditional calls. 
 !!Better used for sent operations.
+!!With throttleing the function runs then a delay appears.
 */
 
 
 
 //Debouncing
+
+function debounce(func, delay) {
+    let debounceTimer
+    return (...args)=>{
+        clearTimeout(debounceTimer)
+        debounceTimer = setTimeout(()=> {
+            func(...args)
+        }, delay)
+    }
+
+}
+ 
+function handleInput(e) {
+    console.log('Input detected from element with id ' + e.target.id)
+}
+
+document.getElementById('name-input').addEventListener('input', debounce(handleInput, 500))
+
+
+
+/*
+Purpose: Verifies that a function is called only after a certain period has passed since
+the last triggering event.
+Mechanism: When an event fires multiple times debouncing delays it's call until
+a certain period has passed since its last call.
+!!Better use for input messages.
+!!With debouncing the dilay appears first the the function runs.
+*/
+
+
+/*
+!! The difference between the two is that throtteling timer start when the function 
+is called and keeps ticking until it ends, then the function can run again. For
+debouncing the timer resets every time the function is called and the function can
+be executed again only when the the timer ends. 
+
+*/
+
+//Debouncing with recursion  
 
 function debounce(func, delay) {
  
@@ -38,7 +78,7 @@ let throttleTimeout = null
 
     return function ret(...args) {
        
-        console.log(throttleTimeout)
+        //console.log(throttleTimeout)
        if(!throttleTimeout) {
                 throttleTimeout = setTimeout(()=> {
                 throttleTimeout = null
@@ -62,24 +102,7 @@ function handleInput(e) {
     console.log('Input detected from element with id ' + e.target.id)
 }
 const deb = debounce(handleInput, 1000)
-document.getElementById('name-input').addEventListener('input', deb)
-
-/*
-Purpose: Verifies that a function is called only after a certain period has passed since
-the last triggering event.
-Mechanism: When an event fires multiple times debouncing delays it's call until
-a certain period has passed since its last call.
-!!Better use for input messages.
-*/
-
-
-/*
-!! The difference between the two is that throtteling timer start when the function 
-is called and keeps ticking until it ends, then the function can run again. For
-debouncing the timer resets every time the function is called and the function can
-be executed again only when the the timer ends. 
-*/
-
+//document.getElementById('name-input').addEventListener('input', deb)
 
 
 
